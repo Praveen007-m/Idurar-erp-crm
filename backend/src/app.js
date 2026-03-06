e34914d628598be98123f73134f74c7773a3b630
Praveen007-m <praveensk3737@gmail.com>
2026-03-06 12:25:14 +0530

    Allow all CORS for deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -24,6 +24,7 @@ app.use(
     origin: "*",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
 
